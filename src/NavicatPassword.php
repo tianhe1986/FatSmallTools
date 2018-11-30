06de03693e889f6ba650e1aa76312643f8c3bd2b
tianhe1986 <w1s2j3229@163.com>
2018-11-30 16:53:43 +0800

    版本12密码处理。
    Signed-off-by: tianhe1986 <w1s2j3229@163.com>

--- src/NavicatPassword.php
+++ src/NavicatPassword.php
@@ -4,9 +4,11 @@ namespace FatSmallTools;
 
 class NavicatPassword
 {
-    protected $version = 11;
+    protected $version = 0;
+    protected $aesKey = 'libcckeylibcckey';
+    protected $aesIv = 'libcciv libcciv ';
     
-    public function __construct($version = 11)
+    public function __construct($version = 12)
     {
         $this->version = $version;
     }
@@ -34,9 +36,10 @@ class NavicatPassword
     }
     protected function encryptTwelve($string)
     {
-        
+        $result = openssl_encrypt($string, 'AES-128-CBC', $this->aesKey, OPENSSL_RAW_DATA, $this->aesIv);
+        return strtoupper(bin2hex($result));
     }
-    
+
     public function decrypt($string)
     {
         $result = FALSE;
@@ -60,6 +63,7 @@ class NavicatPassword
     }
     protected function decryptTwelve($string)
     {
-        
+        $data = hex2bin(strtolower($string));
+        return openssl_decrypt($data, 'AES-128-CBC', $this->aesKey, OPENSSL_RAW_DATA, $this->aesIv);
     }
 }
--- tests/NavicatPasswordTest.php
+++ tests/NavicatPasswordTest.php
@@ -7,5 +7,13 @@ use FatSmallTools\NavicatPassword;
 
 class NavicatPasswordTest extends TestCase
 {
-    
+    public function testHandleTwelve()
+    {
+        $navicatPassword = new NavicatPassword(12);
+        $string = '123456';
+        $encrypted = $navicatPassword->encrypt($string);
+        $this->assertEquals('833E4ABBC56C89041A9070F043641E3B', $encrypted);
+        $decrypted = $navicatPassword->decrypt($encrypted);
+        $this->assertEquals($string, $decrypted);
+    }
 }
