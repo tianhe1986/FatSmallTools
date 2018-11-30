e7ccc4dda2e76dad1d728a3038d77e22abdbd366
tianhe1986 <w1s2j3229@163.com>
2018-11-30 16:05:07 +0800

    初始化提交。
    Signed-off-by: tianhe1986 <w1s2j3229@163.com>

---
+++ src/NavicatPassword.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace FatSmallTools;
+
+class NavicatPassword
+{
+    
+}
---
+++ tests/NavicatPasswordTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FatSmallTools\Tests;
+
+use PHPUnit\Framework\TestCase;
+use FatSmallTools\NavicatPassword;
+
+class NavicatPasswordTest extends TestCase
+{
+    
+}
