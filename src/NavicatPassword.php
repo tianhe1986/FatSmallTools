696f24a2a9e93059f97b0a343ee983307a70f84b
tianhe1986 <w1s2j3229@163.com>
2018-11-30 16:13:37 +0800

    类函数定义。
    Signed-off-by: tianhe1986 <w1s2j3229@163.com>

--- src/NavicatPassword.php
+++ src/NavicatPassword.php
@@ -4,5 +4,62 @@ namespace FatSmallTools;
 
 class NavicatPassword
 {
+    protected $version = 11;
     
+    public function __construct($version = 11)
+    {
+        $this->version = $version;
+    }
+    
+    public function encrypt($string)
+    {
+        $result = FALSE;
+        switch ($this->version) {
+            case 11:
+                $result = $this->encryptEleven($string);
+                break;
+            case 12:
+                $result = $this->encryptTwelve($string);
+                break;
+            default:
+                break;
+        }
+        
+        return $result;
+    }
+    
+    protected function encryptEleven($string)
+    {
+        
+    }
+    protected function encryptTwelve($string)
+    {
+        
+    }
+    
+    public function decrypt($string)
+    {
+        $result = FALSE;
+        switch ($this->version) {
+            case 11:
+                $result = $this->decryptEleven($string);
+                break;
+            case 12:
+                $result = $this->decryptTwelve($string);
+                break;
+            default:
+                break;
+        }
+        
+        return $result;
+    }
+    
+    protected function decryptEleven($string)
+    {
+        
+    }
+    protected function decryptTwelve($string)
+    {
+        
+    }
 }
