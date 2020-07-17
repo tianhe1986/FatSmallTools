c5930bff5cd4bbdce2f321826115835a967a7f02
tianhe1986 <w1s2j3229@163.com>
2020-07-17 12:06:52 +0800

    red black tree, find and delete.
    Signed-off-by: tianhe1986 <w1s2j3229@163.com>

--- src/Set.php
+++ src/Set.php
@@ -21,6 +21,29 @@ class TreeNode
         $this->color = Color::RED;
         $this->left = $this->right = $this->parent = null;
     }
+    
+    public function isOnLeft()
+    {
+        return $this == $this->parent->left;
+    }
+    
+    public function sibling()
+    {
+        if ($this->parent == null) { // no parent, no sibling
+            return null;
+        }
+        
+        if ($this->isOnLeft()) {
+            return $this->parent->right;
+        }
+        
+        return $this->parent->left;
+    }
+    
+    public function hasRedChild()
+    {
+        return ($this->left != null && $this->left->color == Color::RED) || ($this->right != null && $this->right->color == Color::RED);
+    }
 }
 
 class RedBlackTree
@@ -128,6 +151,7 @@ class RedBlackTree
         $rightSon = $node->right;
         
         $node->right = $rightSon->left;
+        $node->right->parent = $node;
         
         // deal parent of node
         $parent = $node->parent;
@@ -139,7 +163,7 @@ class RedBlackTree
             $parent->right = $rightSon;
         }
         
-        $rightSon->parent = $node->parent;
+        $rightSon->parent = $parent;
         
         $node->parent = $rightSon;
         $rightSon->left = $node;
@@ -151,6 +175,7 @@ class RedBlackTree
         $leftSon = $node->left;
         
         $node->left = $leftSon->right;
+        $node->left->parent = $node;
         
         $parent = $node->parent;
         if ($parent === null) {
@@ -161,12 +186,13 @@ class RedBlackTree
             $parent->right = $leftSon;
         }
         
-        $leftSon->parent = $node->parent;
+        $leftSon->parent = $parent;
         
         $node->parent = $leftSon;
         $leftSon->right = $node;
     }
     
+    // fixing double red
     protected function fixViolation(TreeNode $root, TreeNode $node)
     {
         // reference https://www.geeksforgeeks.org/red-black-tree-set-2-insert/
@@ -252,13 +278,237 @@ class RedBlackTree
         $this->fixViolation($this->root, $node);
     }
     
+    // find node that do not have a left child in the subtree of the given node
+    // that is, the minimum value node of the subtree
+    private function successor(TreeNode $x)
+    { 
+        $temp = $x; 
+
+        while ($temp->left != null){
+            $temp = $temp->left;
+        }
+
+        return $temp; 
+    } 
+    
+    // find node that replaces a deleted node in BST 
+    private function bstReplace(TreeNode $x)
+    { 
+        // when node have 2 children 
+        if ($x->left != null && $x->right != null){
+            return $this->successor($x->right);
+        }
+
+        // when leaf 
+        if ($x->left == null && $x->right == null){
+            return null;
+        }
+
+        // when single child 
+        if ($x->left != null) {
+            return $x->left;
+        } else {
+            return $x->right;
+        }
+    } 
+    
+    private function deleteNode(TreeNode $v)
+    {
+        // find the replace node
+        $u = $this->bstReplace($v);
+        
+        // True when u and v are both black
+        $uvBlack = (($u == null || $u->color == Color::BLACK) && $v->color = Color::BLACK);
+        $parent = $v->parent;
+        
+        if ($u == null) { // u is NULL therefore v is leaf 
+            if ($v == $this->root) { // Oh root
+                $this->root = null;
+            } else {
+                if ($uvBlack) {
+                    // u and v both black
+                    // v is leaf, fix double black at v
+                    $this->fixDoubleBlack($v);
+                } else {
+                    // v is red, need do nothing
+                }
+                
+                // delete v from the tree
+                if ($v->isOnLeft()) {
+                    $parent->left = null;
+                } else {
+                    $parent->right = null;
+                }
+            }
+            
+            unset($v);
+            return;
+        }
+        
+        if ($v->left == null || $v->right == null) { // v has only one child
+            if ($v == $this->root) { // v is root， and u is the only child， then u will be the new root
+                $u->parent = null;
+                $this->root = $u;
+                unset($v);
+            } else {
+                // Detach v from tree and move u up
+                if ($v->isOnLeft()) {
+                    $parent->left = $u;
+                } else {
+                    $parent->right = $u;
+                }
+                $u->parent = $parent;
+                unset($v);
+                
+                // if u and v both black, fix double black at u
+                if ($uvBlack) {
+                    $this->fixDoubleBlack($u);
+                } else { // u or v is red, just set u black
+                    $u->color = Color::BLACK;
+                }
+            }
+            
+            return;
+        }
+        
+        // v has 2 children, then swap the value of u and v, recurse delete u
+        $this->swapValues($v, $u);
+        $this->deleteNode($u);
+    }
+    
+    private function fixDoubleBlack(TreeNode $x)
+    {
+        // reach root
+        if ($x == $this->root) {
+            return;
+        }
+        
+        // double black + red = black
+        if ($x->color == Color::RED) {
+            $x->color = Color::BLACK;
+            return;
+        }
+        
+        $sibling = $x->sibling();
+        $parent = $x->parent;
+        
+        if ($sibling == null) { // no sibling, push up
+            $this->fixDoubleBlack($parent);
+        } else {
+            if ($sibling->color == Color::RED) { // sibling red, recolor sibling to black, parent to red, and then rotate, continue fixDoubleBlack
+                $parent->color = Color::RED;
+                $sibling->color = Color::BLACK;
+                
+                if ($x->isOnLeft()) {
+                    $this->rotateLeft($parent);
+                } else {
+                    $this->rotateRight($parent);
+                }
+                
+                $this->fixDoubleBlack($x);
+            } else { // sibling black
+                if ($sibling->hasRedChild()) { // sibling has red child
+                    if ( ! $sibling->isOnLeft()) { // sibling is right child
+                        if ($sibling->right != null && $sibling->right->color == Color::RED) { // red child is right child
+                            // recolor sibling right child to black
+                            $sibling->right->color = Color::BLACK;
+                            // trick, recolor sibling to parent's color, and then always set parent to black
+                            $sibling->color = $parent->color;
+                            
+                            // left rotate parent
+                            $this->rotateLeft($parent);
+                        } else { // red child is left child
+                            // trick, recolor sibling left child to parent's color, and then always set parent to black
+                            $sibling->left->color = $parent->color;
+                            
+                            // right rotate sibling
+                            $this->rotateRight($sibling);
+                            
+                            // left rotate parent
+                            $this->rotateLeft($parent);
+                        }
+                    } else { // sibling is left child, mirror
+                        if ($sibling->left != null && $sibling->left->color == Color::RED) {
+                            $sibling->left->color = Color::BLACK;
+                            $sibling->color = $parent->color;
+                            $this->rotateRight($parent);
+                        } else {
+                            $sibling->right->color = $parent->color;
+                            $this->rotateLeft($sibling);
+                            $this->rotateRight($parent);
+                        }
+                    }
+                    
+                    // since always set parent to black, do it common
+                    $parent->color = Color::BLACK;
+                } else { // 2 black children recolor sibling to red, push up double black to parent
+                    $sibling->color = Color::RED;
+                    $this->fixDoubleBlack($parent);
+                }
+            }
+        }
+    }
+    
+    private function swapValues(TreeNode $v, TreeNode $u)
+    {
+        $temp = $v->data;
+        $v->data = $u->data;
+        $u->data = $temp;
+    }
+    
+    private function search($data)
+    {
+        $temp = $this->root;
+        
+        while ($temp != null) {
+            $compareResult = $this->compareFun($data, $temp->data);
+            
+            if ($compareResult == 0) {
+                break;
+            } else if ($compareResult < 0) {
+                if ($temp->left == null) {
+                    break;
+                } else {
+                    $temp = $temp->left;
+                }
+            } else {
+                if ($temp->right == null) {
+                    break;
+                } else {
+                    $temp = $temp->right;
+                }
+            }
+        }
+        
+        return $temp;
+    }
+    
     public function erase($data)
     {
+        // Searching the node and delete
+        if ($this->root == null) {
+            // Tree is empty 
+            return;
+        }
         
+        $v = $this->search($data);
+        
+        if ($this->compareFun($v->data, $data) != 0) {
+            return;
+        }
+        
+        $this->deleteNode($v);
     }
     
     public function find($data)
     {
+        // Call real search
+        $node = $this->search($data);
         
+        if ($this->compareFun($node->data, $data) == 0) {
+            return $node;
+        }
+        
+        return null;
     }
 }
